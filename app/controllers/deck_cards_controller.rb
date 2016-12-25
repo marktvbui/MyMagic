638377535edd5669158c2a41f3f4225cecbc86d8
Mark Bui <tvyybui@yahoo.com>
2016-12-25 09:33:22 -0800

    built out controller for decks and deck_cards

--- app/controllers/deck_cards_controller.rb
+++ app/controllers/deck_cards_controller.rb
@@ -1,24 +1,46 @@
 class DeckCardsController < ApplicationController
 
-  def index 
+  def index
+    @deckcard = DeckCards.all
   end
 
   def new
+    @deckcard = DeckCards.new
   end
 
   def create
+    @deckcard = DeckCards.new{user.id :current_user}
+    if @user.save
+      flash[:success] = "User Account Created!"
+      render :index
+    else
+      render :create
+    end
   end
-
+  
   def show
+    @deckcard = DeckCards.find_by(id: params[:id])
   end
 
   def edit
+    @deckcard = DeckCards.find_by(id: params[:id])
   end
 
   def update
+    @deckcard = DeckCards.find_by(id: params[:id])
+    @deckcard.assign_attributes(first_name: params["first_name"],
+                           last_name: params["last_name"],
+                           email: params["email"])
+    @deckcard.save
+    flash[:info] = "Current deck has been updated!"
+    render :show
   end
 
   def destroy
+    @deckcard = DeckCards.find_by(id: params[:id])
+    @deckcard.delete
+    flash[:danger] = "Useless card has been deleted"
+    redirect_to '/deck_cards'
   end
 
 end
--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,24 +1,46 @@
 class DecksController < ApplicationController
 
   def index
+    @deck = Decks.all
   end
 
   def new
+    @deck = Decks.new
   end
 
   def create
+    @deck = Decks.new{user.id :current_user}
+    if @deck.save
+      flash[:success] = "Deck has been Created!"
+      render :index
+    else
+      render :create
+    end
   end
   
   def show
+    @deck = Decks.find_by(id: params[:id])
   end
 
   def edit
+    @deck = Decks.find_by(id: params[:id])
   end
 
   def update
+    @deck = Decks.find_by(id: params[:id])
+    @deck.assign_attributes(first_name: params["first_name"],
+                           last_name: params["last_name"],
+                           email: params["email"])
+    @deck.save
+    flash[:info] = "Current deck has been updated!"
+    render :show
   end
 
   def destroy
+    @deck = Decks.find_by(id: params[:id])
+    @deck.delete
+    flash[:danger] = "Useless deck has been deleted"
+    redirect_to '/decks'
   end
 
 
