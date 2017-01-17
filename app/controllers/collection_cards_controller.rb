36129619c55d540d90506c5402c2e0158fc05033
Mark Bui <tvyybui@yahoo.com>
2017-01-16 21:14:13 -0800

    fixed redirect after submitting a card to collections

--- app/controllers/collection_cards_controller.rb
+++ app/controllers/collection_cards_controller.rb
@@ -2,46 +2,44 @@ class CollectionCardsController < ApplicationController
 
   def index
     sort_colum = params[:sort]
-    @collections = current_user.collection_cards
-    @cards = Card.all
+    @collection_cards = current_user.collection_cards
   end
 
   def show
-    @card = Card.find(params[:id])
+    @collection_cards = current_user.collection_cards
   end
 
   def new
-    @collections = CollectionCard.new
+    @collection_cards = current_user.collection_cards
   end
 
   def create 
-    @collections = CollectionCard.find_or_initialize_by(
-                                      user_id: current_user.id,
-                                      card_id: params[:card_id],
-                                      image_url: params[:image_url]
-                                      )
-    if @collections.id
-      @collections.quantity = @collections.quantity + params[:quantity].to_i
+    @collection_cards = CollectionCard.find_or_initialize_by(
+                                                            user_id: current_user.id,
+                                                            card_id: params[:card_id]
+                                                            )
+    if @collection_cards.id
+      @collection_cards.quantity = @collection_cards.quantity + params[:quantity].to_i
     else
-      @collections.quantity = params[:quantity]
+      @collection_cards.quantity = params[:quantity]
     end
 
-    if @collections.save
+    if @collection_cards.save
       flash[:success] = "Card has been added"
-      redirect_to "/collection_cards"
+      redirect_to :back
     else
-      flash[:danger] = @collections.errors.full_messages.join("<br>").html_safe
+      flash[:danger] = @collection_cards.errors.full_messages.join("<br>").html_safe
       render :new
     end
 
   end
 
   def edit
-    @collections = CollectionCard.find_by(id: params[:id])
+    @collection_cards = Card.find_by(id: params[:id])
   end
 
   def update
-    @collections = CollectionCard.find_by(id: params[:id])
+    @collection_cards = Card.find_by(id: params[:id])
     render :index
   end
 
@@ -52,8 +50,7 @@ class CollectionCardsController < ApplicationController
   end
 
   def search
-    @search_term = params[:search]
-    @collections = CollectionCard.where("card_id LIKE ?", "%#{@search_term}%")
+    @cards = Card.where(name: params[:name])
     render :index
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User < ApplicationRecord
   has_many :deck_cards, through: :decks
 
   has_many :collection_cards
-  has_many :cards, through: :collection_cards
 
   has_many :wish_lists
 
